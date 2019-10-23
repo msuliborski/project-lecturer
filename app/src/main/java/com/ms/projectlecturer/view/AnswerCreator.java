f689e2efaad4ea689c7f4b722aa9e76dc91e5ad8
Michał Suliborski <msuliborski@gmail.com>
2019-10-23 23:29:07 +0200

    migration to andoirdx

--- app/src/main/java/com/ms/projectlecturer/view/AnswerCreator.java
+++ app/src/main/java/com/ms/projectlecturer/view/AnswerCreator.java
@@ -1,11 +1,11 @@
 package com.ms.projectlecturer.view;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/ms/projectlecturer/view/RecyclerViewAdapter.java
+++ app/src/main/java/com/ms/projectlecturer/view/RecyclerViewAdapter.java
@@ -1,6 +1,6 @@
 package com.ms.projectlecturer.view;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
