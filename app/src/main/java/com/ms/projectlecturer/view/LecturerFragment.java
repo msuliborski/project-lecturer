9487d94a788952e52f7fe0bbd5e61ef9004621ec
ninjarlz <michal.kusmidrowicz@gmail.com>
2019-11-20 21:16:23 +0100

    lecturer view logic 1

--- app/src/main/java/com/ms/projectlecturer/view/LecturerFragment.java
+++ app/src/main/java/com/ms/projectlecturer/view/LecturerFragment.java
@@ -4,14 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.ms.projectlecturer.R;
+import com.ms.projectlecturer.model.Lecturer;
+import com.ms.projectlecturer.util.RecyclerViewAdapter;
 
-public class LecturerFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LecturerFragment extends Fragment implements RecyclerViewAdapter.ItemClickListener,
+        View.OnClickListener {
+
+
+    //private String lecturerId;
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter adapter;
+    private List<String> dataset;
+    private List<String> presencesIds;
+    private ValueEventListener currentListener;
+    private DatabaseReference lecturerReference;
+    private TextView lecturerStats;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -22,5 +47,50 @@ public class LecturerFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+
+    public void setLecturer(DatabaseReference lecturerReference, String lecturerStats) {
+        this.lecturerStats.setText(lecturerStats);
+        if (currentListener != null) {
+            lecturerReference.removeEventListener(currentListener);
+        }
+        this.lecturerReference = lecturerReference;
+        currentListener = lecturerReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                dataset = new ArrayList<>();
+                presencesIds = new ArrayList<>();
+//               for (DataSnapshot childSnapshot : dataSnapshot.getChildren()) {
+//                     e = childSnapshot.getValue(Enquire.class);
+//                    if (e.getType() == _moduleType) {
+//                        _dataset.add(e.toString());
+//                        _enquiresIDs.add(childSnapshot.getKey());
+//                    }
+//                }
+//                Collections.reverse(_enquiresIDs);
+//                Collections.reverse(_dataset);
+//                _adapter = new RecyclerViewAdapter(_inflater, _dataset);
+//                _recyclerView.setAdapter(_adapter);
+//                _adapter.setClickListener(_moduleItemClickListener);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                databaseError.toException().printStackTrace();
+            }
+        });
+
     }
 }
